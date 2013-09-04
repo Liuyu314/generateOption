1566e298431c14d9e2c39cafb9e730f993d05e65
刘宇 <1017964057@qq.com>
2013-09-04 18:59:44 +0800

    Original Version

--- hello.py
+++
@@ -1,7 +0,0 @@
-# Author: liuyu
-# Email: yuliu314@gmail.com
-# Time: 2013,09,03 22:14:20
-# Description:
-# Version:
-# Option:
-
--- run_test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-genOpt.py ./tests/hello.c
-genOpt.py ./tests/hello.py
-genOpt.py ./tests/hello.scm
